341c8836c0ac04055dc2b9f0ea84f000c776d7e9
Hiten-Tandon <66129059+Hiten-Tandon@users.noreply.github.com>
2024-02-22 00:55:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Distinct occurrences/distinct-occurrences.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Distinct_Occurrences
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String line = sc.nextLine();
+			String S = line.split(" ")[0];
+			String T = line.split(" ")[1];
+			
+			Solution ob = new Solution();
+			System.out.println(ob.subsequenceCount(S,T));
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+
+class Solution
+{
+    
+    private static final int MOD = 1_000_000_007;
+    private static int rec(
+        String s, 
+        String t, 
+        int sPos, 
+        int tPos, 
+        int[][] dp
+    ) {
+        if(tPos == t.length()) {
+            return 1;
+        }
+        
+        if(sPos == s.length()) {
+            return 0;
+        }
+        
+        if(dp[sPos][tPos] == -1) {
+            dp[sPos][tPos] = rec(s, t, sPos + 1, tPos, dp) % MOD;
+            if(s.charAt(sPos) == t.charAt(tPos)) {
+                dp[sPos][tPos] += rec(s, t, sPos + 1, tPos + 1, dp);
+                dp[sPos][tPos] %= MOD;
+            }
+        }
+        
+        return dp[sPos][tPos];
+    }
+    public static int subsequenceCount(String s, String t) {
+        int[][] dp = new int[s.length()][t.length()];
+        
+        for(final int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        return rec(s, t, 0, 0, dp);
+    }
+}
