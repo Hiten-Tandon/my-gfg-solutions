82bca3a887bad014d39f899d50c6c34bfc409773
Hiten-Tandon <66129059+Hiten-Tandon@users.noreply.github.com>
2024-03-01 08:21:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Peak element/peak-element.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+class PeakElement{
+	public static void main(String[] args)throws IOException{
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+			int n=Integer.parseInt(in.readLine().trim());
+			int[] a=new int[n];
+		    int[] tmp=new int[n];
+		    String s[]=in.readLine().trim().split(" ");
+			for(int i=0;i<n;i++){
+				a[i]=Integer.parseInt(s[i]);
+				tmp[i] = a[i];
+			}
+		    
+		    int f=0;
+			int A=(new Solution()).peakElement(tmp,n);
+			
+			if(A<0 && A>=n)
+			    out.println(0);
+			else
+			{
+    			if(n==1 && A==0)
+    			    f=1;
+    			else if(A==0 && a[0]>=a[1])
+    			    f=1;
+    			else if(A==n-1 && a[n-1]>=a[n-2])
+    			    f=1;
+    			else if(A >= 0 && A < n && a[A] >=a[A+1] && a[A]>= a[A-1])
+    			    f=1;
+    			else
+    			    f=0;
+			    out.println(f);
+			}
+		}
+		out.close();
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+	public int peakElement(int[] arr,int n) {
+	    int left = 0;
+	    int right = arr.length;
+	    
+	    while (left < right) {
+	        int mid = left + (right - left) / 2;
+	        
+	        if(mid == arr.length - 1|| arr[mid] > arr[mid + 1]) {
+	            right = mid;
+	        } else {
+	            left = mid + 1;
+	        }
+	    }
+	    
+	    return left;
+	}
+}
